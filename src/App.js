8b24509e91b0d79ec93aae245868bfb90a2f5089
kushalnigam0604 <nigamkushal0@gmail.com>
2023-02-02 00:05:34 +0530

    Add general css to index file

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './Components/Header/Header';
+import Navbar from './Components/Navbar/Navbar';
+import About from './Components/About/About';
+import Experience from './Components/Experience/Experience';
+import Portfolio from './Components/Portfolio/Portfolio';
+import Footer from './Components/Footer/Footer';
+import Contact from './Components/Contact/Contact';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header/>
+      <Navbar/>
+      <About/>
+      <Experience/>
+      <Portfolio/>
+      <Contact/>
+      <Footer/>
+    </>
   );
 }
 
